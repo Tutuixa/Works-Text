6062ab9a36553623be58515be39a961fbdd8fc70
Tutuixa <thuivaz@icloud.com>
2024-11-24 19:13:35 -0300

    Modify the file main.js

--- main.js
+++ main.js
@@ -17,19 +17,3 @@ app.on('activate', () => {
 		createWindow();
 	}
 });
-
-const ffmpeg = require('fluent-ffmpeg');
-
-//Get the paths to the packaged versions of the binaries we want to use
-const ffmpegPath = require('ffmpeg-static').replace(
-	'app.asar',
-	'app.asar.unpacked'
-);
-const ffprobePath = require('ffprobe-static').path.replace(
-	'app.asar',
-	'app.asar.unpacked'
-);
-
-//tell the ffmpeg package where it can find the needed binaries.
-ffmpeg.setFfmpegPath(ffmpegPath);
-ffmpeg.setFfprobePath(ffprobePath);
