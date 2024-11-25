b7639dbecc5afe69d2d085715867b8a70d024456
Tutuixa <thuivaz@icloud.com>
2024-11-24 22:23:16 -0300

    Ajust files

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 let mainWindow = null;
 async function createWindow() {
 	mainWindow = new BrowserWindow({
 		width: 1000,
 		height: 800,
+		titleBarStyle: "customButtonOnHover",
 	});
 
-	await mainWindow.loadFile('./src/pages/notes/index.html');
+	await mainWindow.loadFile("./src/pages/notes/index.html");
 }
 
 app.whenReady().then(createWindow);
 
-app.on('activate', () => {
+app.on("activate", () => {
 	if (BrowserWindow.getAllWindows().length === 0) {
 		createWindow();
 	}
